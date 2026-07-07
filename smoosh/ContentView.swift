e2f65fc3158290608b36b3028570806365375a5a
zieqs <hazieqfarhann@gmail.com>
2026-07-08 00:06:14 +0800

    Initial Commit

---
+++ smoosh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smoosh
+//
+//  Created by zieqs on 08/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smoosh/smooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  smooshApp.swift
+//  smoosh
+//
+//  Created by zieqs on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct smooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
