1d3bd4064ee57f0329d087e2e25cec2c6a25592d
zieqs <hazieqfarhann@gmail.com>
2026-07-09 08:24:27 +0800

    feat: add OptimizationCoordinator for file-type routing

---
+++ smoosh/Services/OptimizationCoordinator.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UniformTypeIdentifiers
+
+final class OptimizationCoordinator {
+    static let shared = OptimizationCoordinator()
+
+    private init() {}
+
+    func optimize(fileAt url: URL, appState: AppState) {
+        guard let utType = UTType(filenameExtension: url.pathExtension) else {
+            return
+        }
+
+        if utType.conforms(to: .pdf) {
+            PDFOptimizationService.shared.optimize(fileAt: url, appState: appState)
+        } else if utType.conforms(to: .image) {
+            ImageOptimizationService.shared.optimize(fileAt: url, appState: appState)
+        }
+    }
+}
